04a2ce3b0253f80941413c34b70499ebea2a4a92
Samuel Kahn <sam@tickr.com>
2021-05-24 20:16:07 -0700

    debugging

--- train.py
+++ train.py
@@ -17,9 +17,11 @@ def collate_fn(batch):
 		hr_inf = torch.isinf(hr).any()
 		lr_inf = torch.isinf(lr).any()
 		good_vals = [hr_nan,lr_nan,hr_inf,lr_inf]
+		print(hr.shape,lr.shape)
 		if hr.shape == (600,600) and lr.shape == (100,100) and True not in good_vals:
 			hrs.append(hr)
 			lrs.append(lr)
+
 	return torch.stack(hrs, dim=0), torch.stack(lrs, dim=0)
 
 
@@ -29,9 +31,19 @@ def main():
 	# Load file paths from config
 	config = configparser.ConfigParser()
 	config.read('paths.config')
+
+	# Configuration Information
 	hst_path = config["DEFAULT"]["hst_path"]
 	hsc_path = config["DEFAULT"]["hsc_path"]
-	
+	model_name = config["MODEL_NAME"]["model_name"]
+
+	comet_tag = config["COMET_TAG"]["comet_tag"]
+
+	batch_size = int(config["BATCH_SIZE"]["batch_size"])
+	srresnet_steps = int(config["SRRESENET_STEPS"]["srresnet_steps"])
+	gan_steps = int(config["GAN_STEPS"]["gan_steps"])
+
+
 	# Adding Comet Logging
 	api_key = os.environ['COMET_ML_ASTRO_API_KEY']
 
@@ -41,13 +53,9 @@ def main():
 	    workspace="samkahn-astro",
 	)
 
-	comet_tag = config["COMET_TAG"]["comet_tag"]
 	experiment.add_tag(comet_tag)
 
 
-
-
-	batch_size = int(config["BATCH_SIZE"]["batch_size"])
 	# Create Dataloader
 	dataloader = torch.utils.data.DataLoader(
 	    SR_HST_HSC_Dataset(hst_path = hst_path , hsc_path = hsc_path, hr_size=[600, 600], 
@@ -58,19 +66,14 @@ def main():
 	# Define Generator
 	generator = Generator(n_res_blocks=16, n_ps_blocks=2,pix_shuffle=False)
 
-	srresnet_steps = int(config["SRRESENET_STEPS"]["srresnet_steps"])
-
 	# Pretrain 
 	generator = train_srresnet(generator, dataloader, device, experiment, lr=1e-6, total_steps=srresnet_steps, display_step=250)
-
-	model_name = config["MODEL_NAME"]["model_name"]
 
 	torch.save(generator, f'srresnet_{model_name}.pt')
 
 	generator = torch.load(f'srresnet_{model_name}.pt')
 	discriminator = Discriminator(n_blocks=1, base_channels=8)
 
-	gan_steps = int(config["GAN_STEPS"]["gan_steps"])
 	generator,discriminator = train_srgan(generator, discriminator, dataloader, device, experiment, lr=1e-6, total_steps=gan_steps, display_step=1000)
 	
 	torch.save(generator, f'srresnet_{model_name}.pt')
